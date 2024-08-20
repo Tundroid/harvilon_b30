a30759ea97a0ad7fb102305372ddeeec3f0ac1dd
Tundroid <felixkimbu@outlook.com>
2024-08-20 12:11:33 +0100

    Adding colorama

--- internet_worker.py
+++ internet_worker.py
@@ -1,70 +1,79 @@
 #!/bin/python3
 
+import base64
+from colorama import init, Fore, Back, Style
 import requests
 import schedule
 import time
+from values import *
 
 
 def is_connected_to_ap():
     try:
-        requests.get('http://192.168.213.1', timeout=5)
+        requests.get(GATEWAY_BASE_URL, timeout=5)
         return True
     except requests.ConnectionError:
         return False
 
 def is_internet_reachable():
     try:
-        requests.get('https://www.google.com', timeout=5)
+        requests.get(INTERNET_CHECK_URL, timeout=5)
         return True
     except requests.ConnectionError:
         return False
 
 def login():
-    data = {'goformId': 'LOGIN', 'password': 'bW9sZWFkbWlu'}
-    response = requests.post('http://192.168.213.1/reqproc/proc_post', data=data)
+    data = {'goformId': USERNAME, 'password': base64.b64encode(PASSWORD)}
+    response = requests.post(f'{GATEWAY_BASE_URL}/reqproc/proc_post', data=data)
     return response.json()['result'] == '0'
 
 def logout():
     data = {'goformId': 'LOGOUT'}
-    requests.post('http://192.168.213.1/reqproc/proc_post', data=data)
+    requests.post(f'{GATEWAY_BASE_URL}/reqproc/proc_post', data=data)
 
 def connect():
-    print("Connecting...")
+    print(Style.BRIGHT + Fore.YELLOW + "Connecting...")
     data = {'goformId': 'CONNECT_NETWORK'}
-    response = requests.post('http://192.168.213.1/reqproc/proc_post', data=data)
+    response = requests.post(f'{GATEWAY_BASE_URL}/reqproc/proc_post', data=data)
     return_value = response.json()['result'] == 'success'
-    print("Successful!" if return_value else "Failed!")
+    print(Style.BRIGHT + Fore.GREEN + "Successful!" if return_value else Style.BRIGHT + Fore.RED + "Failed!")
     return return_value
 
 def disconnect():
-    print("Disconnecting...")
+    print(Style.BRIGHT + Fore.YELLOW + "Disconnecting...")
     data = {'goformId': 'DISCONNECT_NETWORK'}
-    response = requests.post('http://192.168.213.1/reqproc/proc_post', data=data)
+    response = requests.post(f'{GATEWAY_BASE_URL}/reqproc/proc_post', data=data)
     return_value = response.json()['result'] == 'success'
-    print("Successful!" if return_value else "Failed!")
+    print(Style.BRIGHT + Fore.GREEN + "Successful!" if return_value else Style.BRIGHT + Fore.RED + "Failed!")
     return return_value
 
 def reconnect_internet():
     if is_connected_to_ap():
         try:
             if is_internet_reachable():
-                print("Internet is reachable, waiting for next interval...")
+                print(Style.BRIGHT + Fore.GREEN + "Internet is reachable, waiting for next interval...")
             else:
-                print("Internet is not reachable, trying to reconnect...")
+                print(Style.BRIGHT + Fore.RED + "Internet is not reachable, trying to reconnect...")
                 if not disconnect():
-                    print("Failed to disconnect, logging in...")
+                    print(Style.BRIGHT + Fore.RED + "Failed to disconnect, logging in...")
                     if login():
-                        print("Log in successful...")
+                        print(Style.BRIGHT + Fore.GREEN + "Log in successful...")
                         disconnect()
+                    else:
+                        print(Style.BRIGHT + Fore.RED + "Log in failed, check credentials...")
+                        return
                 connect()
         except requests.ReadTimeout:
-            print("Timeout occurred, waiting for next interval...")
+            print(Style.BRIGHT + Fore.YELLOW + "Timeout occurred, waiting for next interval...")
     else:
-        print("Not connected to AP, waiting for next interval...")
+        print(Style.BRIGHT + Fore.YELLOW + "Not connected to AP, waiting for next interval...")
 
-# Run the script every 10 seconds
-schedule.every(3).seconds.do(reconnect_internet)
 
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+if __name__ == "__main__":
+    init(autoreset=True)
+    # Run the script every 10 seconds
+    schedule.every(3).seconds.do(reconnect_internet)
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
